9ff1af70e216360b0db5a1d0778563e4b0d4bce9
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2017-10-30 10:23:35 -0400

    First program

--- 1-Fundamentals/0-Notes.java
+++ 1-Fundamentals/0-Notes.java
@@ -1,4 +1,27 @@
 
+// This file won't compile... just for notes. //
+
+// Structure of Java functions
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+HelloWorld // is a class, and also the filename;
+main // is a method, the main method for an executable Java file (e.g. init)
+
+// Class names are written in PascalCase
+// Method names are lowerCamelCase, and usually verbs
+
+public // is an access modifier. public means it's accessible from any other class or method in our project
+
+static // means the method belongs to and is called from the class rather than from an instance of the class
+
+void // indicates this method (the main() method) doesn't return anything
+
+// 
 
 public class Variables {
     public static void main(String args[]){
@@ -17,3 +40,4 @@ public class Variables {
         
     }
 }
+
