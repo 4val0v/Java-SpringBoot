5479253e1c64ba99de1b46f4accec744154f972a
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2017-10-31 18:14:53 -0400

    Created objectmaster game

---
+++ Projects/src/com/codingdojo/objectmaster/Human.java
@@ -0,0 +1,22 @@
+package com.codingdojo.objectmaster;
+
+class Human {
+	private int strength = 3;
+	private int stealth = 3;
+	private int intelligence = 3;
+	private int health = 100;
+	
+	public Human() {}
+	
+	public void attack(Human attacked) {
+		attacked.setHealth(attacked.getHealth() - this.strength);
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+}
