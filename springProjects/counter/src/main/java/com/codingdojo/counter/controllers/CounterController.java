a85e75203d8f730cee275ec16cfd7350593df7e2
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2017-11-02 21:36:40 -0400

    Add session counter

---
+++ springProjects/counter/src/main/java/com/codingdojo/counter/controllers/CounterController.java
@@ -0,0 +1,34 @@
+package com.codingdojo.counter.controllers;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CounterController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L; // not sure what this does, but apparently it's important
+	
+	@RequestMapping("/")
+	public String index(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws ServletException, IOException {
+		if(session.getAttribute("counter") == null) { // if "counter" not in session, add attribute and set to 0;
+			session.setAttribute("counter", 0);
+		}
+		session.setAttribute("counter", (int)session.getAttribute("counter") + 1); // increment session counter
+		return "index";
+	}
+	
+	@RequestMapping("/show")
+	public String index(Model model, HttpServletRequest request, HttpServletResponse response, HttpSession session) throws ServletException, IOException {
+		model.addAttribute("counter", session.getAttribute("counter")); // show session counts on page
+		return "show";
+	}
+
+}
---
+++ springProjects/counter/src/main/webapp/WEB-INF/show.jsp
@@ -0,0 +1,23 @@
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Home Page</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+<div class=container>
+
+<div class=row>
+<div class="col-md-12 text-center">
+        <p>&nbsp;</p>
+        <p>You have visited <a href="/">this site</a> <c:out value="${counter}"/> times.</p>
+</div>
+
+</div>
+</body>
+</html>
